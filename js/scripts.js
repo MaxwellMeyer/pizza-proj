ce441b9c07321eb9906903efd3ed8458b76b0383
Maxwell Meyer <maxreadswell@gmail.com>
2021-06-18 10:27:41 -0700

    begin writing prototype methods

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,18 @@ function Pizza(price, toppings, size) {
   this.toppings = toppings;
   this.size = size;
 }
+Pizza.prototype.size = function() {
+  let pizzaSize = this.size;
+  let pizzaPrice = 0;
+  this.price = 0;
+  switch(pizzaSize) {
+    case "family":
+      pizzaPrice = 21000;
+      break;
+    case "large":
+      pizzaPrice = 2750;
+      break;
+    case "medium":
+    pizzaPrice = 2500;
+      break;
+  }
