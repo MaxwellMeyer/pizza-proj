4296990a4399498b2be92c65d00707cb71b16981
Maxwell Meyer <maxreadswell@gmail.com>
2021-06-18 14:12:58 -0700

    fix bug with method naming conventions

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ function Pizza(price, toppings, size) {
   this.toppings = toppings;
   this.size = size;
 }
-Pizza.prototype.size = function () {
+Pizza.prototype.calcSize = function () {
   let pizzaSize = this.size;
   let pizzaPrice = 0;
   this.price = 0;
@@ -20,10 +20,34 @@ Pizza.prototype.size = function () {
   }
   return (this.price += pizzaPrice);
 };
-
-Pizza.prototype.toppings = function () {
+Pizza.prototype.calcToppings = function () {
   this.toppings.forEach((topping) => {
     this.price += 100;
   });
   return this.price;
 };
+$(document).ready(function () {
+  $("#pizzaForm").submit(function (event) {
+    event.preventDefault();
+    const pizzaToppings = [];
+    const pizzaPrice = 0;
+    const pizzaSize = $("input:radio[name=size]:checked").val();
+    $("input:checkbox[name=top]:checked").each(function () {
+      pizzaToppings.push(parseInt($(this).val()));
+    });
+    let food = new Pizza(0, pizzaToppings, pizzaSize);
+    food.calcSize();
+    food.calcToppings();
+    $("#pizzaCost").text(food.price);
+    $("#results").fadeIn();
+    $(".jumbotron").fadeOut();
+    $(".rsButton").fadeIn();
+  });
+  $(".again").click(function (event) {
+    event.preventDefault();
+    $("#results").fadeOut();
+    $(".rsButton").fadeOut();
+    $(".jumbotron").fadeIn();
+    $("#pizzaForm")[0].reset();
+  });
+});
